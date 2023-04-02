43f2f97a67100474349f0a19b5fbaf126da7116c
Harsh Gupta <harsh.gupta2211@gmail.com>
2023-04-03 01:10:59 +0530

    update files

--- pages/profile.js
+++ pages/profile.js
@@ -1,12 +1,12 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-// @material-ui/icons
-import Camera from "@material-ui/icons/Camera";
-import Palette from "@material-ui/icons/Palette";
-import Favorite from "@material-ui/icons/Favorite";
+
 // core components
 import Header from "../components/Header/Header.js";
 import Footer from "../components/Footer/Footer.js";
@@ -23,6 +23,11 @@ const useStyles = makeStyles(styles);
 
 export default function ProfilePage(props) {
   const classes = useStyles();
+
+  const [nftList, setNFTList] = useState([]);
+  const [nftImage, setNFTImage] = useState([]);
+  const [nftData, setNFTData] = useState([]);
+
   const { ...rest } = props;
   const imageClasses = classNames(
     classes.imgRaised,
@@ -30,20 +35,71 @@ export default function ProfilePage(props) {
     classes.imgFluid
   );
   const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+
+  const parseURL = async (url) => {
+    const ipfsHash = url.slice(7);
+    const ipfsURL = `https:/${ipfsHash}`;
+    const data = await fetch(ipfsURL);
+    const json = await data.json();
+    console.log(json);
+    return json;
+  };
+
+  const getImage = (url) => {
+    let image = url;
+    image = image.toString();
+    console.log(image.slice(7, 66));
+    // https://bafyreiearcor5tvq7jiqsmx6teotmcaibqn7k6waqigibnemxzif5pjvsq.ipfs.nftstorage.link/metadata.json
+    return "https://" + image.slice(7, 66) + ".ipfs.nftstorage.link/image.jpg";
+  };
+  async function fetchData() {
+    try {
+      const addr = localStorage.getItem("walletAddress");
+
+      const response = await axios.get(`/api/ERC721Transfers`, {
+        params: {
+          owner: addr
+        }
+      });
+      console.log("Response:",response);
+      console.log(response.data.allNFTs.length);
+      var dataList = [];
+      var imgList = [];
+      for (var i = 0; i < response.data.allNFTs.length; i++) {
+        var data = await parseURL(response.data.allNFTs[i].tokenURI);
+        var image = getImage(data.image);
+        console.log(data);
+        dataList.push(data);
+        imgList.push(image);
+      }
+      setNFTList(response.data.allNFTs);
+      setNFTData(dataList);
+      console.log(dataList);
+      setNFTImage(imgList);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Header
         color="transparent"
-        brand="NextJS Material Kit"
+        brand="MintBoxx 📦"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
           height: 200,
-          color: "white"
+          color: "white",
         }}
         {...rest}
       />
-      <Parallax small filter image="/img/profile-bg.jpg" />
+
+      <Parallax image="https://static.vecteezy.com/system/resources/previews/005/406/681/original/rocket-in-hole-vector.jpg"></Parallax>
       <div className={classNames(classes.main, classes.mainRaised)}>
         <div>
           <div className={classes.container}>
@@ -52,151 +108,35 @@ export default function ProfilePage(props) {
                 <div className={classes.profile}>
                   <div>
                     <img
-                      src="/img/faces/christian.jpg"
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLDERscs5d-zl6ao7Gp7c1nRJlSVFtWpzT3t180tFNg_UP_YLx1YXMOOMLJGCasvy0gho&usqp=CAU"
                       alt="..."
                       className={imageClasses}
                     />
                   </div>
-                  <div className={classes.name}>
-                    <h3 className={classes.title}>Christian Louboutin</h3>
-                    <h6>DESIGNER</h6>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-twitter"} />
-                    </Button>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-instagram"} />
-                    </Button>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-facebook"} />
-                    </Button>
-                  </div>
                 </div>
               </GridItem>
             </GridContainer>
-            <div className={classes.description}>
-              <p>
-                An artist of considerable range, Chet Faker — the name taken by
-                Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-                and records all of his own music, giving it a warm, intimate
-                feel with a solid groove structure.{" "}
-              </p>
-            </div>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-                <NavPills
-                  alignCenter
-                  color="primary"
-                  tabs={[
-                    {
-                      tabButton: "Studio",
-                      tabIcon: Camera,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-1.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-2.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-5.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-4.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    },
-                    {
-                      tabButton: "Work",
-                      tabIcon: Palette,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/olu-eletu.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/clem-onojeghuo.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/cynthia-del-rio.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/mariya-georgieva.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/clem-onojegaw.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    },
-                    {
-                      tabButton: "Favorite",
-                      tabIcon: Favorite,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/mariya-georgieva.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-3.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src="/img/examples/clem-onojeghuo.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/olu-eletu.jpg"
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src="/img/examples/studio-1.jpg"
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    }
-                  ]}
-                />
-              </GridItem>
-            </GridContainer>
+            {nftData.length > 0 && (
+              <GridContainer justify="center">
+                <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
+                  <GridContainer justify="center">
+                    {nftList.map((item, key) => (
+                      <>
+                        <GridItem xs={12} sm={12} md={6}>
+        
+                          <h6>{nftData[key].name}</h6>
+                          <img
+                            alt="..."
+                            src={nftImage[key]}
+                            className={navImageClasses}
+                          />
+                        </GridItem>
+                      </>
+                    ))}
+                  </GridContainer>
+                </GridItem>
+              </GridContainer>
+            )}
           </div>
         </div>
       </div>
